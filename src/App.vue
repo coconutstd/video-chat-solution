f9c1e28fb25ef2b9e86f997b2b93901241ff1ead
junui <wnsspdl@naver.com>
2020-11-29 23:46:27 +0900

    add auth

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div v-if="authState !== 'signedin'">You are signed out.</div>
+    <amplify-authenticator>
+      <div v-if="authState === 'signedin' && user">
+        <div>Hello, {{user.username}}</div>
+      </div>
+      <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'AuthStateApp',
+  created() {
+    onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
+  data() {
+    return {
+      user: undefined,
+      authState: undefined
+    }
+  },
+  beforeDestroy() {
+    return onAuthUIStateChange;
   }
 }
 </script>
