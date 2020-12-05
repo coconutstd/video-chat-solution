fad54b327fae60c64c76c2bea1566fc63c727d10
junui <wnsspdl@naver.com>
2020-12-05 19:04:33 +0900

    빌드 테스트2

--- src/VideoChat.vue
+++ src/VideoChat.vue
@@ -510,6 +510,8 @@ name: "VideoChat",
     }
   },
   mounted() {
+
+
     var VueDom = this
     var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
       function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
