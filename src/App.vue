396cd302af5aa4d0e997902128f10a153b2538ab
junui <wnsspdl@naver.com>
2020-12-09 01:39:58 +0900

    UI SignIn 한글로 변경, 테마 색 변경

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <amplify-authenticator>
       <ToolBar></ToolBar>
       <router-view v-bind:user="user"></router-view>
+    <amplify-sign-in
+        header-text="옴니프로젝트"
+        username-alias="username"
+        slot="sign-in"
+        :form-fields.prop="signInFormFields"
+    ></amplify-sign-in>
     <amplify-sign-out></amplify-sign-out>
   </amplify-authenticator>
 
@@ -24,7 +30,22 @@ export default {
   data() {
     return {
       user: undefined,
-      authState: undefined
+      authState: undefined,
+      signInFormFields: [
+        {
+          type: 'username',
+          label: '아이디',
+          placeholder: '아이디를 입력해주세요',
+          required: true,
+        },
+        {
+          type: 'password',
+          label: '비밀번호',
+          placeholder: '비밀번호를 입력해주세요',
+          required: true,
+        }
+      ]
+
     }
   },
   beforeDestroy() {
@@ -41,5 +62,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+:root {
+  --amplify-primary-color: #ff6347;
+  --amplify-primary-tint: #ff7359;
+  --amplify-primary-shade: #e0573e;
 }
 </style>
